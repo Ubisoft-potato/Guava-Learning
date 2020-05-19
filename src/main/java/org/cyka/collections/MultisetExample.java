cfb0792273cda225e528000fea8cfa2e77d7de6f
cyka <1354250064mdzz@gmail.com>
2020-05-19 11:05:58 +0800

    MultisetExample

---
+++ src/main/java/org/cyka/collections/MultisetExample.java
@@ -0,0 +1,33 @@
+package org.cyka.collections;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @ClassName MultisetExample @Description Multiset Usage @Author long @Date 2020/5/14
+ * 18:54 @Version 1.0
+ */
+@Slf4j
+public class MultisetExample {
+  public static void main(String[] args) {
+    bookstoreHashMultiset();
+  }
+
+  private static void bookstoreHashMultiset() {
+    Multiset<String> bookStore = HashMultiset.create();
+    bookStore.add("Potter");
+    bookStore.add("Potter");
+    bookStore.add("Potter");
+
+    log.info("Potter count: {}", bookStore.count("Potter"));
+
+    bookStore.add("Tom", 6);
+    log.info("Tom count:{}", bookStore.count("Tom"));
+
+    bookStore
+        .entrySet()
+        .forEach(
+            entry -> log.info("entry element: {}, size: {}", entry.getElement(), entry.getCount()));
+  }
+}
